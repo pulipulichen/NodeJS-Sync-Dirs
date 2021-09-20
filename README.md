f8bf67765fe84c941e7577a996911651bde20447
Pulipuli Chen <blog@pulipuli.info>
2021-09-20 23:30:14 +0800

    0920 建構到一半，今天先這樣

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # NodeJS-Sync-Dirs
 A script to sync source directory to target directory.
+
+# TODO
+
+1. 取得a目錄底下的檔案
+2. 
--- sync-dirs.js
+++ sync-dirs.js
@@ -5,14 +5,82 @@ const handleFileFromArgvArray = require('./lib/cli/handleFileFromArgvArray.js')
 const execShellCommand = require('./lib/cli/execShellCommand.js')
 
 const path = require('path')
+const fs = require('fs')
+
+const getFilelistFromFolder = require('./lib/fileList/getFilelistFromFolder.js')
 
 let main = function () {
 
   handleFileFromArgvArray({
     lockKey: false
-  },(argvArray) => {
+  },async (argvArray) => {
     
-    console.log(argvArray)
+    //console.log(argvArray)
+    
+    let sourceFolder = argvArray[0]
+    let targetFolder = argvArray[1]
+    
+    // --------------------------------
+    
+    if (sourceFolder.endsWith('/') || sourceFolder.endsWith('\\')) {
+      sourceFolder = sourceFolder.slice(0, -1)
+    }
+    
+    if (targetFolder.endsWith('/') || targetFolder.endsWith('\\')) {
+      targetFolder = targetFolder.slice(0, -1)
+    }
+    
+    // --------------------------------
+    
+    if (fs.existsSync(sourceFolder) === false) {
+        //return continue
+        return false
+        //continue
+      }
+      
+      if (fs.lstatSync(sourceFolder).isDirectory() === false) {
+        throw Error(sourceFolder + ' should be a directory.')
+      }
+    
+    // --------------------------------
+    
+    if (fs.existsSync(targetFolder) === false) {
+      //return continue
+      return false
+      //continue
+    }
+
+    if (fs.lstatSync(targetFolder).isDirectory() === false) {
+      throw Error(targetFolder + ' should be a directory.')
+    }
+    
+    // --------------------------------
+    
+    let sourceList = await getFilelistFromFolder(sourceFolder)
+    //console.log(sourceList)
+    
+    // ---------------------------------
+    
+    sourceList.forEach((sourceFile) => {
+      //console.log(sourceFile)
+      
+      // 我是否已經處理過這個檔案了？
+      
+      
+      // -----------------------
+      
+      // 取得基本路徑
+      let relativePath = sourceFile.slice(sourceFolder.length)
+      //console.log(relativePath)
+      
+      let targetFile = targetFolder + relativePath
+      let fileFolder = path.dirname(targetFile)
+      
+      console.log(fileFolder)
+      
+      // ---------------------
+    })
+    
     /*
     if (!file) {
       execShellCommand(`wine "${appPath}"`)
